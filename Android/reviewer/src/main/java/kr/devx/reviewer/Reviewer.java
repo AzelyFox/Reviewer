2520d5e5feab925716c5ded16475d3d959de799e
LimeCake <limecake23@gmail.com>
2019-11-11 01:03:08 +0900

    Android library 1.0.0

---
+++ Android/reviewer/src/main/java/kr/devx/reviewer/Reviewer.java
@@ -0,0 +1,75 @@
+package kr.devx.reviewer;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.util.JsonReader;
+import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Reviewer {
+    public static void newReviewGUI(Activity activity, String key, double marketMoveMinRate, String marketPackage) {
+
+    }
+
+    public static void newReview(String key, Review review) {
+        ContentValues values = new ContentValues();
+        values.put("service_key", key);
+        values.put("review_user", review.getUser());
+        values.put("review_tag", review.getTag());
+        values.put("review_rating", review.getRating());
+        values.put("review_title", review.getTitle());
+        values.put("review_content", review.getContent());
+        NetworkTask netTask = new NetworkTask("https://api.devx.kr/Reviewer/v1/review_new.php", values);
+        netTask.execute();
+    }
+
+    static class NetworkTask extends AsyncTask<Void, Void, String> {
+        private String url;
+        private ContentValues values;
+
+        NetworkTask(String url, ContentValues values) {
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result;
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values);
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject result = new JSONObject(s);
+                int resultCode = result.getInt("result");
+                if (resultCode == 0) {
+                    Log.i("DEVX-REVIEWER","SUCCESS : REVIEW ADDED : " + result.getInt("review_index"));
+                } else {
+                    if (resultCode == -1) {
+                        Log.e("DEVX-REVIEWER","FAIL : " + result.getInt("error"));
+                        Log.e("DEVX-REVIEWER","FAIL : " + result.getInt("error_debug"));
+                    }
+                    if (resultCode == -2) {
+                        Log.e("DEVX-REVIEWER","FAIL : " + result.getInt("error"));
+                        Log.e("DEVX-REVIEWER","FAIL : " + result.getInt("error_debug"));
+                    }
+                    if (resultCode == -3) {
+                        Log.e("DEVX-REVIEWER","FAIL : " + result.getInt("error"));
+                        Log.e("DEVX-REVIEWER","FAIL : " + result.getInt("error_debug"));
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("DEVX-REVIEWER","ERROR : " + s);
+            }
+        }
+    }
+}
